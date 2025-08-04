d68a13ad9cf2d1001478502ecf7b10524079f0a7
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-08-04 16:30:46 +0530

    chore: remove test-validation files

--- test-validation/exercises/01_basics/go.mod
+++
@@ -1,3 +0,0 @@
-module goforgo/01_basics/hello
-
-go 1.24
--- test-validation/exercises/01_basics/hello.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-// TODO: Fix this program to print "Hello, GoForGo!"
-func main() {
-	fmt.Println("Hello, World!")
-}
